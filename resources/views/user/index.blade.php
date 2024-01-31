2db431be226fd952bfe814057a54092d13688785
pedro gones <pedrotargin@gmail.com>
2024-01-31 19:15:15 -0300

    changed position of alert box

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.template')
 
 @section('content')
-<head>
-    @include('shared.message')
-</head>
+
 <div class=" px-5 py-2 lg:flex lg:items-center lg:justify-between">
     <div class=" flex justify-between min-w-0 flex-1">
         <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Usuários</h2>
@@ -12,6 +10,7 @@
         </a>
     </div>
 </div>
+@include('shared.message')
 <div class=" px-4 flex flex-col mt-6">
     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
