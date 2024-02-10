8c35a1b2ec0741d6e107692c5925b649d152e241
pedro gones <pedrotargin@gmail.com>
2024-02-10 19:43:44 -0300

    today

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Posts</h2>
-        <p class="mt-2 text-lg leading-8 text-gray-600">Dashboard to view all posts createds</p>
+        <p class="mt-2 text-lg leading-8 text-gray-600">Posts publisheds</p>
       </div>
       <div style="border-radius: 20px" class="mt-10 grid max-w-2xl grid-cols-3 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
